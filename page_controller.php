0ee60bdbc6ccaab36f644af63a48cc35cb679f7b
Andrew Brown <ajbpn7@mst.edu>
2011-08-30 00:05:59 -0500

    random tomfoolery

--- page_controller.php
+++ page_controller.php
@@ -1,5 +1,8 @@
 <?php
 
+require_once('html_controller.php');
+$html = new HtmlController();
+
 class PageController {
   function __construct($page) {
     $this->show($page);
@@ -7,6 +10,7 @@ class PageController {
 
   // Here be dragons
   function show($page) {
+    // If they've requested a page that actually exists:
     if (method_exists($this, $page)) {
       $this->$page();
     } else {
@@ -15,11 +19,20 @@ class PageController {
   }
 
   function home() {
-    echo 'sup';
+    echo $html->header('Rolla Coders: Challenges');
+    if ($logged_in) {
+      // List all challenges for the user
+      $this->challenges();
+    } else {
+      // Offer register/login links before presenting challenges
+      echo $html->link('Click here to register', '?page=register');
+      echo $html->link('Click here to log in', '?page=login');
+    }
   }
 
   function register() {
-    
+    echo $html->header('Rolla Coders: Challenges');
+    echo $html->subheader('Register an account');
   }
 
   function login() {
